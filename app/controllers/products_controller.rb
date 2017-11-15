9a5824450c5c930e021e7798b47fef0040c83400
Tyler Damme <tyler.damme@gmail.com>
2017-11-14 19:50:47 -0600

    add individual pages for first two items

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,4 +4,14 @@ class ProductsController < ApplicationController
     products = Product.all
     render json: products.as_json
   end
+
+  def first_product_method
+    first_product = Product.first
+    render json: first_product.as_json
+  end
+
+  def civilization_product_method
+    civilization = Product.find_by(name: "Civilization VI")
+    render json: civilization.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get "/all_products" => "products#all_products_method"
+  get "/first_product" => "products#first_product_method"
+  get "/civilization" => "products#civilization_product_method"
 end
