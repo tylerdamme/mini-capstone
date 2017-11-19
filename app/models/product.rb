11167e0b06afc0ee15d52f0706e69ebc3fd1f6de
Tyler Damme <tyler.damme@gmail.com>
2017-11-19 11:51:09 -0600

    Add is_discounted, tax, and total methods to Product, change as_json for Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,31 @@
 class Product < ApplicationRecord
+  def is_discounted
+    if price.to_i < 20
+      true
+    else
+      false
+    end
+  end
+
+  def tax
+    (price.to_i * 0.09.to_f).round(2)
+  end
+
+  def total
+    price.to_i + tax
+  end
+
+  def as_json
+    {
+    name: name,
+    console: console,
+    description: description,
+    image: image,
+    price: price,
+    tax: tax,
+    total: total,
+    is_discounted: is_discounted
+  }
+  end
+
 end
