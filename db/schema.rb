030eaf9d01454b5f59dd0bffe15804b79eeba4ae
Tyler Damme <tyler.damme@gmail.com>
2017-11-20 20:27:03 -0600

    Change Price and decription data types using migrate files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171115003647) do
+ActiveRecord::Schema.define(version: 20171121022329) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,9 +18,9 @@ ActiveRecord::Schema.define(version: 20171115003647) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.string "console"
-    t.string "price"
+    t.decimal "price", precision: 9, scale: 2
     t.string "image"
-    t.string "description"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
