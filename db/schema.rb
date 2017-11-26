f3d72ebeb684d7cf3ea57a45810004dded61a172
Tyler Damme <tyler.damme@gmail.com>
2017-11-26 10:44:14 -0600

    Add Supplier model, seed data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121030014) do
+ActiveRecord::Schema.define(version: 20171126162004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 20171121030014) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "instock", default: true
+    t.integer "supplier_id"
+  end
+
+  create_table "suppliers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,10 @@
 
-product = Product.new({name: "Super Mario Odyssey", console: "Nintendo Switch", price: "59", image: "https://media.nintendo.com/nintendo/bin/SmENxSXqVepjMBF5NxYddvUr6gSfLeDn/qSG2l7_qvitLVncMwzncZbBoqANeYYL6.png", description: "New Mario game for the Switch"})
-product.save
-Product.create({name: "Civilization VI", console: "PC", price: "49", image: "https://upload.wikimedia.org/wikipedia/en/3/3b/Civilization_VI_cover_art.jpg", description: "The sixth edition of the Sid Meier classic"})
-Product.create({name: "Zelda: Breath of the Wild", console: "Nintendo Switch", price: "59", image: "https://cdn.vox-cdn.com/uploads/chorus_asset/file/7802443/NintendoSwitch_TLOZBreathoftheWild_boxart.jpg", description: "New Zelda game for the Switch"})
+# product = Product.new({name: "Super Mario Odyssey", console: "Nintendo Switch", price: "59", image: "https://media.nintendo.com/nintendo/bin/SmENxSXqVepjMBF5NxYddvUr6gSfLeDn/qSG2l7_qvitLVncMwzncZbBoqANeYYL6.png", description: "New Mario game for the Switch"})
+# product.save
+# Product.create({name: "Civilization VI", console: "PC", price: "49", image: "https://upload.wikimedia.org/wikipedia/en/3/3b/Civilization_VI_cover_art.jpg", description: "The sixth edition of the Sid Meier classic"})
+# Product.create({name: "Zelda: Breath of the Wild", console: "Nintendo Switch", price: "59", image: "https://cdn.vox-cdn.com/uploads/chorus_asset/file/7802443/NintendoSwitch_TLOZBreathoftheWild_boxart.jpg", description: "New Zelda game for the Switch"})
+
+# Supplier.create({name: "Nintendo", email: "nintendo@email.com", phone_number: "222-222-2222"})
+# Supplier.create({name: "Fraxis", email: "fraxis@email.com", phone_number: "999-999-9999"})
+# Supplier.create({name: "EA Sports", email: "easports@email.com", phone_number: "888-888-8888"})
+# Supplier.create({name: "Microsoft", email: "microsoft@email.com", phone_number: "111-111-1111"})
